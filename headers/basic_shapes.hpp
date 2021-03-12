31ca75e5852d38c9c37bde12380d7a267175f5f8
tylercchase <tylercchase@gmail.com>
2021-03-12 01:13:13 -0900

    Polygon working

--- headers/basic_shapes.hpp
+++ headers/basic_shapes.hpp
@@ -4,30 +4,36 @@
 #include "shape.hpp"
 #include <fstream>
 
-class Circle : public Shape{
-    private:
-        double _radius;
-    public:
-        Circle(const double &radius);
-        void draw(std::ofstream &file) const;
+class Circle : public Shape
+{
+private:
+    double _radius;
+
+public:
+    Circle(const double &radius);
+    void draw(std::ofstream &file) const;
 };
 
-class Polygon : public Shape{
-    private:
-        int _numSides;
-        double sideLength;
-    public:
-        Polygon(const int & numberSides, const double & sideLength);
-        void draw(std::ofstream &file) const;
+class Polygon : public Shape
+{
+private:
+    int _numSides;
+    double _sideLength;
+
+public:
+    Polygon(const int &numberSides, const double &sideLength);
+    void draw(std::ofstream &file) const;
 };
 
-class Rectangle : public Shape{
-    private:
-        double _width;
-        double _height;
-    public:
-        Rectangle(const double & width, const double & height);
-        void draw(std::ofstream &file) const;
+class Rectangle : public Shape
+{
+private:
+    double _width;
+    double _height;
+
+public:
+    Rectangle(const double &width, const double &height);
+    void draw(std::ofstream &file) const;
 };
 
 // class Spacer : public Shape{
--- tests.cpp
+++ tests.cpp
@@ -4,7 +4,8 @@
 #include <fstream>
 #include <math.h>
 
-std::string readFile(){
+std::string readFile()
+{
     std::string buildOutput;
     std::string line;
     std::ifstream test_file("build_test.ps");
@@ -24,31 +25,31 @@ TEST_CASE("Draw a circle.")
     ofs.close();
     REQUIRE(readFile() == "newpath\n144 144 144 0 360 arc \nstroke\n");
 }
-TEST_CASE("Make sure a polygon has the correct size sides") {
-    Polygon polygon1(4,4.0);
-    
+TEST_CASE("Make sure a polygon has the correct size sides")
+{
+    Polygon polygon1(4, 4.0);
+
     REQUIRE(abs(polygon1.getHeight() - 4.0 < 0.01));
     REQUIRE(abs(polygon1.getWidth() - 4.0 < 0.01));
-    
-
 }
 
-TEST_CASE("Draw a rectangle") {
+TEST_CASE("Draw a rectangle")
+{
     std::ofstream ofs;
     ofs.open("build_test.ps");
-    Rectangle rectangle(50,100);
+    Rectangle rectangle(50, 100);
     rectangle.draw(ofs);
     ofs.close();
     REQUIRE(readFile() == "newpath\n25 50 50 100 rectstroke \nstroke\n");
 }
 
-// TEST_CASE("Draw a polygon.")
-// {
-//     std::ofstream ofs;
-//     ofs.open("build_test.ps");
-//     Polygon polygon(144);
-//     polygon.draw(ofs);
-//     ofs.close();
+TEST_CASE("Draw a polygon.")
+{
+    std::ofstream ofs;
+    ofs.open("build_test.ps");
+    Polygon polygon(4, 100);
+    polygon.draw(ofs);
+    ofs.close();
 
-//     REQUIRE(readFile() == "Correct output goes here");
-// }
+    REQUIRE(readFile() == "Correct output goes here");
+}
